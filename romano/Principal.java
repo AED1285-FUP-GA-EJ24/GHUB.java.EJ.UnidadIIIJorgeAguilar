96f8f40053e0ce9930788eae5c4f9dee2c37c26f
Jorge Emilio Aguilar Guerrero <159278343+JorgeEmilioAguilarGuerrero@users.noreply.github.com>
2024-04-16 15:01:53 +0000

    Clase Romano

---
+++ romano/Principal.java
@@ -0,0 +1,7 @@
+package romano;
+public class Principal {
+    public static void main(String[] args) {
+        Romano objRomano = new Romano();
+        objRomano.romano();
+    }
+}
---
+++ romano/Romano.java
@@ -0,0 +1,19 @@
+//Proposito: Convertir un numero arabigo del 1 al 10 
+//			en Romano I -1 2-II 3-III 4-IV 5-V
+//						6-VI 7-VII 8-VIII 9-IX 10-X
+//Autor: Jorge Aguilar
+//Fecha 16/abr/2024
+package romano;
+import java.util.Scanner;
+public class Romano {
+    public void romano(){
+        //Definir objeto Scanner
+        Scanner teclado = new Scanner(System.in);
+        //Definir variable de salida
+        char roma;
+        //Definir variable de entrada
+        byte ara;
+        System.out.println("Proporciona número arábigo(1-10): ");
+        ara = teclado.nextByte();
+    }
+}
