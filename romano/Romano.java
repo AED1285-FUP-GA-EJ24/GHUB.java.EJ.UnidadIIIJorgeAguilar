ead47638fb51b70a19feb19141d054c4744035d5
Jorge Emilio Aguilar Guerrero <159278343+JorgeEmilioAguilarGuerrero@users.noreply.github.com>
2024-04-17 14:39:24 +0000

    Actualizacion Clase Romano

--- romano/Romano.java
+++ romano/Romano.java
@@ -10,10 +10,46 @@ public class Romano {
         //Definir objeto Scanner
         Scanner teclado = new Scanner(System.in);
         //Definir variable de salida
-        char roma;
+        String roma;
         //Definir variable de entrada
-        byte ara;
+        short ara;
         System.out.println("Proporciona número arábigo(1-10): ");
-        ara = teclado.nextByte();
+        ara = teclado.nextShort();
+        switch (ara) {
+            case 1:
+                roma ="I";
+                break;
+            case 2:
+                roma ="II";
+                break;
+            case 3:
+                roma ="III";
+                break;
+            case 4:
+                roma ="IV";
+                break;
+            case 5:
+                roma ="V";
+                break;
+            case 6:
+                roma ="VI";
+                break;
+            case 7:
+                roma ="VII";
+                break;
+            case 8:
+                roma ="VIII";
+                break;
+            case 9:
+                roma ="IX";
+                break;
+            case 10:
+                roma ="X";
+                break;
+            default:
+                roma = "Valor Invalido";
+                break;
+        }
+        System.out.println("Romano = "+roma);
     }
 }
