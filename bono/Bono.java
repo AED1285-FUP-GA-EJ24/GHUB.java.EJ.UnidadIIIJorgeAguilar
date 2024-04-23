34f845f0b45d342fd51c95a3a4b6ee87298d1bb4
Jorge Emilio Aguilar Guerrero <159278343+JorgeEmilioAguilarGuerrero@users.noreply.github.com>
2024-04-23 14:51:24 +0000

    Bono Segunda Version

--- bono/Bono.java
+++ bono/Bono.java
@@ -20,6 +20,7 @@ public class Bono {
         double importe_bonificacion, importe_venta;
         System.out.print("Proporciona Ventas del Mes:");
         importe_venta = teclado.nextDouble();
+        /* 
         if (importe_venta>0 && importe_venta<=1000){
             importe_bonificacion = 0;
         }else if(importe_venta>1000 && importe_venta<=5000){
@@ -31,8 +32,18 @@ public class Bono {
         }
         System.out.println("Bonificacion = $"+importe_bonificacion);
         teclado.close();
-       
-
+       */
+      if (importe_venta>20000){
+        importe_bonificacion = importe_venta * 8 / 100;
+      }else if(importe_venta>5000){
+        importe_bonificacion = importe_venta * 5 / 100;
+      }else if (importe_venta>1000){
+        importe_bonificacion = importe_venta * 3 / 100;
+      }else{
+        importe_bonificacion = 0;
+      }
+      System.out.println("Bonificacion = $"+importe_bonificacion);
+      teclado.close();
     }
-    
+      
 }
